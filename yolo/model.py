d5d7028e44685cf36d9f144820937688e45e1b4a
YuhangJi <>
2021-01-25 13:41:04 +0800

    upgrade

--- yolo/model.py
+++ yolo/model.py
@@ -5,40 +5,40 @@ from yolo._layers import ConvBN, SeparateResidualBlock, UpSampleLayer, YOLOHead
 
 class NetBackbone(tf.keras.models.Model, ABC):
 
-    def __init__(self, input_shape, alpha=0.1,use_bias=False):
+    def __init__(self, input_shape, alpha=0.1,use_bias=False,beta=16):
         super(NetBackbone, self).__init__()
         self.h, self.w, self.c = input_shape
 
         self.input_layer = tf.keras.layers.Input(shape=(self.h, self.w, self.c))
-        self.base_block = ConvBN(16,kernel_size=(3,3), strides=1, alpha=alpha,use_bias=use_bias)
-        self.conv_block1 = ConvBN(32,kernel_size=(3,3), alpha=alpha,strides=2)
-        self.block1_residual1 = SeparateResidualBlock(32,alpha=alpha)
-        self.conv_block2 = ConvBN(64,kernel_size=(3,3), alpha=alpha,strides=2)
-        self.block2_residual1 = SeparateResidualBlock(64,alpha=alpha)
-        self.block2_residual2 = SeparateResidualBlock(64,alpha=alpha)
-        self.conv_block3 = ConvBN(128,kernel_size=(3,3), alpha=alpha,strides=2)
-        self.block3_residual1 = SeparateResidualBlock(128, alpha=alpha)
-        self.block3_residual2 = SeparateResidualBlock(128, alpha=alpha)
-        self.block3_residual3 = SeparateResidualBlock(128, alpha=alpha)
-        self.block3_residual4 = SeparateResidualBlock(128, alpha=alpha)
-        self.block3_residual5 = SeparateResidualBlock(128, alpha=alpha)
-        self.block3_residual6 = SeparateResidualBlock(128, alpha=alpha)
-        self.block3_residual7 = SeparateResidualBlock(128, alpha=alpha)
-        self.block3_residual8 = SeparateResidualBlock(128, alpha=alpha)
-        self.conv_block4 = ConvBN(256,kernel_size=(3,3), alpha=alpha,strides=2)
-        self.block4_residual1 = SeparateResidualBlock(256, alpha=alpha)
-        self.block4_residual2 = SeparateResidualBlock(256, alpha=alpha)
-        self.block4_residual3 = SeparateResidualBlock(256, alpha=alpha)
-        self.block4_residual4 = SeparateResidualBlock(256, alpha=alpha)
-        self.block4_residual5 = SeparateResidualBlock(256, alpha=alpha)
-        self.block4_residual6 = SeparateResidualBlock(256, alpha=alpha)
-        self.block4_residual7 = SeparateResidualBlock(256, alpha=alpha)
-        self.block4_residual8 = SeparateResidualBlock(256, alpha=alpha)
-        self.conv_block5 = ConvBN(512,kernel_size=(3,3), alpha=alpha,strides=2)
-        self.block5_residual1 = SeparateResidualBlock(512, alpha=alpha)
-        self.block5_residual2 = SeparateResidualBlock(512, alpha=alpha)
-        self.block5_residual3 = SeparateResidualBlock(512, alpha=alpha)
-        self.block5_residual4 = SeparateResidualBlock(512, alpha=alpha)
+        self.base_block = ConvBN(beta*(2**0),kernel_size=(3,3), strides=1, alpha=alpha,use_bias=use_bias)
+        self.conv_block1 = ConvBN(beta*(2**1),kernel_size=(3,3), alpha=alpha,strides=2)
+        self.block1_residual1 = SeparateResidualBlock(beta*(2**1),alpha=alpha)
+        self.conv_block2 = ConvBN(beta*(2**2),kernel_size=(3,3), alpha=alpha,strides=2)
+        self.block2_residual1 = SeparateResidualBlock(beta*(2**2),alpha=alpha)
+        self.block2_residual2 = SeparateResidualBlock(beta*(2**2),alpha=alpha)
+        self.conv_block3 = ConvBN(beta*(2**3),kernel_size=(3,3), alpha=alpha,strides=2)
+        self.block3_residual1 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.block3_residual2 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.block3_residual3 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.block3_residual4 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.block3_residual5 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.block3_residual6 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.block3_residual7 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.block3_residual8 = SeparateResidualBlock(beta*(2**3), alpha=alpha)
+        self.conv_block4 = ConvBN(beta*(2**4),kernel_size=(3,3), alpha=alpha,strides=2)
+        self.block4_residual1 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.block4_residual2 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.block4_residual3 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.block4_residual4 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.block4_residual5 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.block4_residual6 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.block4_residual7 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.block4_residual8 = SeparateResidualBlock(beta*(2**4), alpha=alpha)
+        self.conv_block5 = ConvBN(beta*(2**5),kernel_size=(3,3), alpha=alpha,strides=2)
+        self.block5_residual1 = SeparateResidualBlock(beta*(2**5), alpha=alpha)
+        self.block5_residual2 = SeparateResidualBlock(beta*(2**5), alpha=alpha)
+        self.block5_residual3 = SeparateResidualBlock(beta*(2**5), alpha=alpha)
+        self.block5_residual4 = SeparateResidualBlock(beta*(2**5), alpha=alpha)
         self.output_layer = self.call(self.input_layer)
 
     def build(self, input_shape=None):
@@ -71,24 +71,25 @@ class NetBackbone(tf.keras.models.Model, ABC):
 
 
 class Net(tf.keras.models.Model, ABC):
-    def __init__(self,input_shape,out_channels,alpha=0.1):
+    def __init__(self,input_shape,out_channels,alpha=0.1,beta=16):
         super(Net, self).__init__()
         self.h,self.w,self.channel = input_shape
         self.alpha = alpha
+        self.beta = beta
 
         # Input Layer
         self.input_layer = tf.keras.layers.Input(shape=input_shape)
 
         # Backbone
-        self.base_model = NetBackbone(input_shape)
+        self.base_model = NetBackbone(input_shape,beta=self.beta)
 
-        self.yolo_head_13 = YOLOHead(filters=512, out_channels=out_channels, alpha=self.alpha)
-        self.up_sampling_13 = UpSampleLayer(filters=256, alpha=self.alpha)
+        self.yolo_head_13 = YOLOHead(filters=self.beta*(2**5), out_channels=out_channels, alpha=self.alpha)
+        self.up_sampling_13 = UpSampleLayer(filters=self.beta*(2**4), alpha=self.alpha)
 
-        self.yolo_head_26 = YOLOHead(filters=256, out_channels=out_channels, alpha=self.alpha)
-        self.up_sampling_26 = UpSampleLayer(filters=128, alpha=self.alpha)
+        self.yolo_head_26 = YOLOHead(filters=self.beta*(2**4), out_channels=out_channels, alpha=self.alpha)
+        self.up_sampling_26 = UpSampleLayer(filters=self.beta*(2**3), alpha=self.alpha)
 
-        self.yolo_head_52 = YOLOHead(filters=128, out_channels=out_channels, alpha=self.alpha)
+        self.yolo_head_52 = YOLOHead(filters=self.beta*(2**3), out_channels=out_channels, alpha=self.alpha)
 
         # Output Layer
         self.output_layer = self.call(self.input_layer)
@@ -120,7 +121,7 @@ if __name__ == "__main__":
     原始YOLOv3一共61646349个参数, DarkNet 一共40620640个参数.
     """
     # net = NetBackbone(input_shape=(416,416,3))
-    net = Net(input_shape=(416, 416, 3), out_channels=3 * (5 + 14))
+    net = Net(input_shape=(416, 416, 3), out_channels=3 * (5 + 14),beta=4)
     net.build()
     net.summary()
 
